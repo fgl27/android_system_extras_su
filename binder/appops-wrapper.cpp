0a0e52f35b11c463a054d3e138356c6068668f7f
fgl27 <fglfgl27@gmail.com>
2020-01-23 22:53:41 -0300

    Workaround to have SU back working witout app ops
    Signed-off-by: fgl27 <fglfgl27@gmail.com>

--- binder/appops-wrapper.cpp
+++ binder/appops-wrapper.cpp
@@ -9,22 +9,16 @@ extern "C" {
 
 int appops_start_op_su(int uid, const char* pkgName) {
     ALOGD("Checking whether app [uid:%d, pkgName: %s] is allowed to be root", uid, pkgName);
-
     AppOpsManager ops;
-    int mode = ops.startOpNoThrow(AppOpsManager::OP_SU, uid, String16(pkgName), false);
-    if (mode == AppOpsManager::MODE_ALLOWED) {
-        ALOGD("Privilege elevation allowed by appops");
-        return 0;
-    }
-
-    ALOGD("Privilege elevation denied by appops");
-    return 1;
+    ops.startOpNoThrow(AppOpsManager::OP_NONE, uid, String16(pkgName), false);
+    ALOGD("Privilege elevation allowed by appops");
+    return 0;
 }
 
 void appops_finish_op_su(int uid, const char* pkgName) {
     ALOGD("Finishing su operation for app [uid:%d, pkgName: %s]", uid, pkgName);
     AppOpsManager* ops = new AppOpsManager();
-    ops->finishOp(AppOpsManager::OP_SU, uid, String16(pkgName));
+    ops->finishOp(AppOpsManager::OP_NONE, uid, String16(pkgName));
     delete ops;
 }
 }
